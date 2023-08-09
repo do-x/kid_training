ce4ee5a798f3e07f76e7dc67c81e669052547035
do-x <765296028@qq.com>
2023-08-09 09:55:48 -0400

    x.x

--- dcpx/dcpx.c
+++ dcpx/dcpx.c
@@ -2,31 +2,38 @@
 #include <string.h>
 
 int main( int argc, char *argv[] ){
-	int s=1, aaa, *temp;
+	int s=1;
+	char temp[20];
+	char temp2[20];
+
+	while(s){
+	s=0;
 	for(int i=1; i<argc; i++){
 		for(int j=0; j<strlen(argv[i]); j++){
 			if((i+1)>=argc){
 				break;
 			}
-			printf("i=%d, j=%d\n", i, j);
-			printf("%c, %c\n", argv[i][j], argv[i+1][j]);	
-		}	
+			if((int)argv[i][j]>(int)argv[i+1][j]){
+				/*
+				printf("i=%d, j=%d\n", i, j);
+				printf("%c, %c\n", argv[i][j], argv[i+1][j]);
+				printf("%d, %d\n", (int)argv[i][j], (int)argv[i+1][j]);
+				*/
+				strcpy(temp, argv[i]);
+				strcpy(argv[i], argv[i+1]);
+				strcpy(argv[i+1], temp);
+				//printf("%s\n", argv[i]);
+				s++;
+				break;
+				}
+			}
+		}
 	}
-
 	for(int i=1; i<argc; i++){
 		if((i+1)>=argc){
 			break;
 		}
-		printf("%s, %s\n", argv[i], argv[i+1]);
-		printf("i=%d\n", i);
-		if(strlen(argv[i])<strlen(argv[i+1])){
-			memcpy(temp, argv[i], strlen(argv[i+1]));
-			memcpy(argv[i], argv[i+1], strlen(argv[i+1]));
-			memcpy(argv[i+1], temp, strlen(argv[i+1]));
-
-			printf("%s, %s\n", argv[i], argv[i+1]);
-		}
-		printf("%s, %s\n", argv[i], argv[i+1]);
+		printf("%s\n", argv[i]);
 	}
 
 	return 0;
