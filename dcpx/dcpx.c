e74bc2efc9598a9af133eaf6e153ccbbc0fa7c3e
do-x <765296028@qq.com>
2023-08-10 10:49:47 -0400

    reorder20230810

--- dcpx/dcpx.c
+++ dcpx/dcpx.c
@@ -5,35 +5,48 @@ int main( int argc, char *argv[] ){
 	int s=1;
 	char temp[20];
 	char temp2[20];
+	int temp3;
+
 
 	while(s){
-	s=0;
-	for(int i=1; i<argc; i++){
-		for(int j=0; j<strlen(argv[i]); j++){
-			if((i+1)>=argc){
-				break;
-			}
-			if((int)argv[i][j]>(int)argv[i+1][j]){
-				/*
+		s=0;
+		for(int i=1; i<argc; i++){
+			for(int j=0; j<strlen(argv[i]); j++){
+				if((i+1)>=argc){
+					break;
+				}
+				if((i-1)==0){
+					break;	
+				}
 				printf("i=%d, j=%d\n", i, j);
 				printf("%c, %c\n", argv[i][j], argv[i+1][j]);
-				printf("%d, %d\n", (int)argv[i][j], (int)argv[i+1][j]);
-				*/
-				strcpy(temp, argv[i]);
-				strcpy(argv[i], argv[i+1]);
-				strcpy(argv[i+1], temp);
-				//printf("%s\n", argv[i]);
-				s++;
-				break;
+				printf("%d, %d\n\n", (int)argv[i][j], (int)argv[i+1][j]);
+				
+				if((int)argv[i-1][j]>(int)argv[i][j]){
+					strcpy(temp2, argv[i-1]);
+					strcpy(argv[i-1], argv[i]);
+					strcpy(argv[i], temp2);
+					break;
+					s++;
+				}
+				if((int)argv[i][j]>(int)argv[i+1][j]){
+					strcpy(temp, argv[i]);
+					strcpy(argv[i], argv[i+1]);
+					strcpy(argv[i+1], temp);
+					
+					break;
+					s++;
 				}
 			}
 		}
 	}
+	
+	
 	for(int i=1; i<argc; i++){
-		if((i+1)>=argc){
+		if((i+1)>argc){
 			break;
 		}
-		printf("%s\n", argv[i]);
+		printf("\n%s\n", argv[i]);
 	}
 
 	return 0;
