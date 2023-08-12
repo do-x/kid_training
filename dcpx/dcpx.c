eae898795b8345ed337c73a289fb724c6915d094
do-x <765296028@qq.com>
2023-08-12 08:46:31 -0400

    jinbu

--- dcpx/dcpx.c
+++ dcpx/dcpx.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main( int argc, char *argv[] ){
 	int s=1;
 	char temp[20];
 	char temp2[20];
 	int temp3;
-
 
 	while(s){
 		s=0;
@@ -21,30 +21,42 @@ int main( int argc, char *argv[] ){
 				printf("i=%d, j=%d\n", i, j);
 				printf("%c, %c\n", argv[i][j], argv[i+1][j]);
 				printf("%d, %d\n\n", (int)argv[i][j], (int)argv[i+1][j]);
-				
-				if((int)argv[i-1][j]>(int)argv[i][j]){
-					strcpy(temp2, argv[i-1]);
-					strcpy(argv[i-1], argv[i]);
-					strcpy(argv[i], temp2);
+				if((int)argv[i][j]>(int)argv[i+1][j]){
+					printf("argv[i]=%s, argv[i+1]=%s, temp=%s\n\n", argv[i], argv[i+1], temp);
+					strcpy(temp, argv[i]);
+					printf("temp=argv[i]\n");
+					printf("argv[i]=%s, argv[i+1]=%s, temp=%s\n\n", argv[i], argv[i+1], temp);
+					strcpy(argv[i], argv[i+1]);
+					printf("argv[i]=argv[i+1]\n");
+					printf("argv[i]=%s, argv[i+1]=%s, temp=%s\n\n", argv[i], argv[i+1], temp);
+					strcpy(argv[i+1], temp);
+					printf("argv[i+1]=temp\n");
+					printf("argv[i]=%s, argv[i+1]=%s, temp=%s\n", argv[i], argv[i+1], temp);
 					break;
 					s++;
-				}
-				if((int)argv[i][j]>(int)argv[i+1][j]){
-					strcpy(temp, argv[i]);
-					strcpy(argv[i], argv[i+1]);
-					strcpy(argv[i+1], temp);
-					
-					break;
+					if((int)argv[i-1][j]>(int)argv[i][j]){
+						printf("argv[i-1]=%s, argv[i]=%s, temp=%s\n\n", argv[i-1], argv[i], temp2);
+						strcpy(temp2, argv[i-1]);
+						printf("temp2=argv[i-1]\n");
+						printf("argv[i-1]=%s, argv[i]=%s, temp=%s\n\n", argv[i-1], argv[i], temp2);
+						strcpy(argv[i-1], argv[i]);
+						printf("argv[i-1]=argv[i]\n");
+						printf("argv[i-1]=%s, argv[i]=%s, temp=%s\n\n", argv[i-1], argv[i], temp2);
+						strcpy(argv[i], temp2);
+						printf("argv[i]=temp2\n");
+						printf("argv[i-1]=%s, argv[i]=%s, temp=%s\n", argv[i-1], argv[i], temp2);
+						break;
+						s++;
+					}
 					s++;
 				}
 			}
 		}
 	}
-	
-	
+
 	for(int i=1; i<argc; i++){
 		if((i+1)>argc){
-			break;
+			break;	
 		}
 		printf("\n%s\n", argv[i]);
 	}
